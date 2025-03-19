aff9cc0bb8a161ab02f8c4a9cdd5729c7bab7711
ravisingh87 <ravisingh19870903@gmail.com>
2025-03-19 15:50:56 +0530

    Error Handling and introduced match function

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -4,18 +4,20 @@ fn main() {
     enable_raw_mode().unwrap(); // We enter raw mode with this method
     for b in io::stdin().bytes(){
         // These two lines basically print out the character that was read
-        let b = b.unwrap(); // Variable Shadowing
-        // println!("{}",b); //  byte representation
-        let c = b as char;
-        // println!("{}",c);
-        if c.is_control(){
-            println!("Binary: {0:08b} ASCII: {0:#03} \r",b);
-        }else{
-            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",b,c);
-        }
-        if c == 'q'{
-            disable_raw_mode().unwrap(); // Here we disable raw mode again
-            break;
+        match b{
+            Ok(b)=> {
+                let c = b as char;
+                if c.is_control(){
+                    println!("Binary: {0:08b} ASCII: {0:#03} \r",b);
+                }else{
+                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",b,c);
+                }
+                if c == 'q'{
+                    break;
+                }
+            }
+            Err(err)=> println!("{}",err),
         }
     }
+    disable_raw_mode().unwrap(); // Here we disable raw mode again
 }
