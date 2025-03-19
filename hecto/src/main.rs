c7131decd36d23b3be8ad3af4c79f36fd7dd871e
ravisingh87 <ravisingh19870903@gmail.com>
2025-03-19 11:43:52 +0530

    Implemented raw mode from cannonical mode by adding dependency of crossterm

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{self,Read}; // from standard library importing Input and output stuff(io)
+use crossterm::terminal::{enable_raw_mode,disable_raw_mode}; 
 fn main() {
-    println!("Hello, world!");
+    enable_raw_mode().unwrap(); // We enter raw mode with this method
+    for b in io::stdin().bytes(){
+        // These two lines basically print out the character that was read
+        let c = b.unwrap() as char;
+        println!("{}",c);
+        if c == 'q'{
+            disable_raw_mode().unwrap(); // Here we disable raw mode again
+            break;
+        }
+    }
 }
