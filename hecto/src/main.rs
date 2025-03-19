84520268f07b7e2084def83102ab13148e41bdae
ravisingh87 <ravisingh19870903@gmail.com>
2025-03-19 16:04:17 +0530

    Refactoring the code

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,23 +1,7 @@
-use std::io::{self,Read}; // from standard library importing Input and output stuff(io)
-use crossterm::terminal::{enable_raw_mode,disable_raw_mode}; 
+mod editor;
+use editor::Editor;
+ 
 fn main() {
-    enable_raw_mode().unwrap(); // We enter raw mode with this method
-    for b in io::stdin().bytes(){
-        // These two lines basically print out the character that was read
-        match b{
-            Ok(b)=> {
-                let c = b as char;
-                if c.is_control(){
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r",b);
-                }else{
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",b,c);
-                }
-                if c == 'q'{
-                    break;
-                }
-            }
-            Err(err)=> println!("{}",err),
-        }
-    }
-    disable_raw_mode().unwrap(); // Here we disable raw mode again
+    let editor = Editor::default();
+    editor.run();
 }
