3ee1d6a31cacfe20ad85710a9f3c3fb4e4276a87
ravisingh87 <ravisingh19870903@gmail.com>
2025-03-19 18:06:45 +0530

    Implemented tild on the left of terminal

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,39 +1,64 @@
-use crossterm::event::{read,Event::Key,KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+mod terminal;
+use terminal::Terminal;
 
 pub struct Editor{
     should_quit:bool,
 }
 
 impl Editor{
-    pub fn default() -> Self{
-        Editor{ should_quit:false}
+    pub const fn default() -> Self{
+        Self { should_quit: false }
     }
 
     pub fn run(&mut self){
-        if let Err(err) = self.repl(){
-            panic!("{err:#?}");
-        }
-        println!("Goodbye.\r\n");
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::termnate().unwrap();
+        result.unwrap();
     }
     
     fn repl(&mut self)-> Result<(),std::io::Error>{
-        enable_raw_mode()?; // We enter raw mode with this method
         loop {
-            if let Key(KeyEvent{ code, modifiers,kind,state}) = read()?{
-                println!("Code: {code:?} Modifiers:{modifiers:?} Kind: {kind:?} State: {state:?} \r");
-                match code {
-                    Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => (),
+         self.refresh_screen()?;
+         if self.should_quit{
+            break;
+         }   
+         let event = read()?;
+         self.evaluate_event(&event);
+        }
+        Ok(()) // Here we disable raw mode again
+    }
+    fn evaluate_event(&mut self,event: &Event){
+        if let Key(KeyEvent{
+            code,modifiers,..
+        }) = event{
+            match code{
+                Char('q') if *modifiers == KeyModifiers::CONTROL =>{
+                    self.should_quit = true;
                 }
-            }
-            if self.should_quit{
-                break;
+                _ => (),
             }
         }
-    disable_raw_mode()?;
-    Ok(()) // Here we disable raw mode again
+    }
+    fn refresh_screen(&self)-> Result<(),std::io::Error>{
+        if self.should_quit{
+            Terminal::clear_screen()?;
+            println!("Goodbye.\r\n");
+        }else{
+            Self::draw_row()?;
+            Terminal::move_cursor(0, 0)?;
+        }
+        Ok(())
+    }
+    fn draw_row()-> Result<(),std::io::Error>{
+        let height = Terminal::size()?.1;
+        for cursor_row in 0..height{
+            print!("~");
+            if cursor_row + 1 < height{
+                print!("\r\n");
+            }
+        }
+        Ok(())
     }
 } 
---
+++ hecto/src/editor/terminal.rs
@@ -0,0 +1,30 @@
+use crossterm::cursor::MoveTo;
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode,enable_raw_mode,size,Clear,ClearType};
+use std::io::stdout;
+
+pub struct Terminal{}
+
+impl Terminal{
+    pub fn termnate() -> Result<(),std::io::Error>{
+        disable_raw_mode()?;
+        Ok(())
+    }
+    pub fn initialize() -> Result<(),std::io::Error>{
+        enable_raw_mode()?;
+        let _ = Self::clear_screen();
+        let _ = Self::move_cursor(0,0);
+        Ok(())
+    }
+    pub fn clear_screen() -> Result<(),std::io::Error>{
+        execute!(stdout(),Clear(ClearType::All))?;
+        Ok(())
+    }
+    pub fn move_cursor(x:u16,y:u16) -> Result<(),std::io::Error>{
+        execute!(stdout(),MoveTo(x,y))?;
+        Ok(())
+    }
+    pub fn size() -> Result<(u16,u16),std::io::Error>{
+        size()
+    }
+}
