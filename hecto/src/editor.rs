6b0822729097afc0e1d3e08c574f7dc5fda36a6b
ravisingh87 <ravisingh19870903@gmail.com>
2025-03-19 16:15:49 +0530

    replace for to loop

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self,Read};  // from standard library importing Input and output stuff(io)
+use crossterm::event::{read,Event::Key,KeyCode::Char};
 use crossterm::terminal::{enable_raw_mode,disable_raw_mode};
 
 pub struct Editor{}
@@ -9,23 +9,23 @@ impl Editor{
     }
     pub fn run(&self){
         enable_raw_mode().unwrap(); // We enter raw mode with this method
-    for b in io::stdin().bytes(){
-        // These two lines basically print out the character that was read
-        match b{
-            Ok(b)=> {
-                let c = b as char;
-                if c.is_control(){
-                    println!("Binary: {0:08b} ASCII: {0:#03} \r",b);
-                }else{
-                    println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r",b,c);
-                }
-                if c == 'q'{
-                    break;
-                }
+        loop {
+            match read() {
+                Ok(Key(event))=>{
+                    println!("{:?} \r",event);
+                    match (event.code) {
+                        Char(c)=>{
+                            if c == 'q'{
+                                break;
+                            }
+                        },
+                        _=>(),                        
+                    }
+                },
+                Err(err) => println!("{}",err),
+                _=>()
             }
-            Err(err)=> println!("{}",err),
         }
-    }
     disable_raw_mode().unwrap(); // Here we disable raw mode again
     }
 } 
